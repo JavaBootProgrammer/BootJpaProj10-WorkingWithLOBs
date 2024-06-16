87207b8992c917aa7166993110d0911ebd384eaa
anand8533 <anand8533@gmail.com>
2024-06-16 17:36:29 -0400

    1

---
+++ src/main/java/com/nt/service/IMarriageProfileMgmtService.java
@@ -0,0 +1,11 @@
+package com.nt.service;
+
+import java.util.Optional;
+
+import com.nt.entity.MarriageProfile;
+
+public interface IMarriageProfileMgmtService {
+	public String registerMarriageProfile(MarriageProfile profile);
+
+	public Optional<MarriageProfile> showProfileById(int id);
+}
